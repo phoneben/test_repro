a8b1238fe2cdf69dccefa3575eb2124db1c316d5
phoneben <67923255+phoneben@users.noreply.github.com>
2023-09-15 07:22:40 +0300

    Update weekly_stats.sh

--- weekly_stats.sh
+++ weekly_stats.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-# Initialize the count variable
 tail -7 synthetic_historical_fc_accuracy.tsv >scratch.txt
 cut -d $'\t' -f6 scratch.txt
 week_fc=$(cut -d $'\t' -f6 scratch.txt)
@@ -12,43 +10,40 @@ do
 $ if ((diff < 0)); then let diff*=-1; fi
 $ echo $diff 
 done
-count=3
-lines_in_file="$(wc -l < synthetic_historical_fc_accuracy.tsv)"
-echo $lines_in_file
-loop_no=$(("$lines_in_file"-2))
-for ((i = 1; i <= $loop_no; i++));
-#for i in (0..$(echo wc -l rx_poc.log))
-do
-yesterday_line_count_number="$count"
-today_line_count_number=$(($count-1))
 
-yesterday_line_count=$(sed -n "${yesterday_line_count_number}p" rx_poc.log)
-today_line_count=$(sed -n "${today_line_count_number}p" rx_poc.log)
 
-yesterday_fc=$(echo "$yesterday_line_count"  | cut -d $'\t' -f 5)
-today_temp=$(echo "$today_line_count"| cut -d $'\t' -f 4)
-echo "$yesterday_fc - $today_temp"
-accuracy=$(("$yesterday_fc"-"$today_temp"))
-if [ -1 -le $accuracy ] && [ $accuracy -le 1 ]
-then
-   accuracy_range=excellent
-elif [ -2 -le $accuracy ] && [ $accuracy -le 2 ]
-then
-    accuracy_range=good
-elif [ -3 -le $accuracy ] && [ $accuracy -le 3 ]
-then
-    accuracy_range=fair
-else
-    accuracy_range=poor
-fi
-echo "Forecast accuracy is $accuracy_range"
+echo $(tail -7 synthetic_historical_fc_accuracy.tsv  | cut -f6) > scratch.txt
 
-#row=$($today_line_count)
-year=$(echo "$today_line_count" | cut -d $'\t' -f1)
-month=$(echo "$today_line_count" | cut -d $'\t' -f2)
-day=$(echo "$today_line_count" | cut -d $'\t' -f3)
-echo -e "$year\t$month\t$day\t$today_temp\t$yesterday_fc\t$accuracy\t$accuracy_range" >> historical_fc_accuracy.tsv
- # Increment the count
-    ((count++))
-    echo $count
+week_fc=($(echo $(cat scratch.txt)))
+
+# validate result:
+for i in {0..6}; do
+    echo ${week_fc[$i]}
 done
+
+for i in {0..6}; do
+  if (( ${week_fc[$i]} < 0 ))
+  # if ((diff < 0)); then let diff*=-1; fi
+  then
+ echo "minus" ${week_fc[$i]}
+    week_fc[$i]=$(((-1)*week_fc[$i]))
+  fi
+  # validate result:
+  echo "new" ${week_fc[$i]}
+done
+
+minimum=${week_fc[1]}
+maximum=${week_fc[1]}
+for item in ${week_fc[@]}; do
+   if [[ $minimum > $item ]]
+   then
+     minimum=$item
+   fi
+   if [[ $maximum < $item ]]
+   then
+     maximum=$item
+   fi
+done
+
+echo "minimum ebsolute error = $minimum"
+echo "maximum absolute error = $maximum"
